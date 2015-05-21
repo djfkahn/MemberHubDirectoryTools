ab23be5f38577512b72fb6d6450730378cdddc4e
djfkahn <daniel.jf.kahn@gmail.com>
2015-05-21 13:36:48 -0700

    added CreateFromRoster

--- family.py
+++ family.py
@@ -12,21 +12,10 @@ class Family:
         self.adults   = []
         self.children = []
     
-    def AddAdultFromDirectory(self, fields):
-        new_adult = person.Person()
-        self.adults += [new_adult.SetFromDirectory(fields)]
-    
-    def AddAdultsFromCombinedField(self, fields):
-        # for elementary school (< 6th grade) teacher name is retained
-        # for middle school, teacher name is replaced with grade level
-        if int(fields[2]) < 6:
-            teacher = fields[4]
-        else:
-            teacher = fields[2]
-
+    def AddAdultsFromCombinedField(self, teacher, name_field, grade):
         parent_count = 1
         parent_num = ""
-        parents = fields[3].split(" and ")
+        parents = name_field.split(" and ")
         # if parents have same last name, then there is only one name
         # before the first "and"
         if len(parents[0].split(" ")) == 1:
@@ -36,9 +25,9 @@ class Family:
                 new_adult = person.Person()
                 self.adults += [new_adult.SetFromRoster(last_name  = last_name,
                                                         first_name = parent[0],
-                                                        grade      = fields[2],
+                                                        grade      = grade,
                                                         teacher    = teacher,
-                                                        name_field = fields[3],
+                                                        name_field = name_field,
                                                         family_relation = "Adult"+parent_num)]
                 # prepare the parent_tag for the next parent
                 parent_count += 1
@@ -51,18 +40,42 @@ class Family:
                 new_adult = person.Person()
                 self.adults += [new_adult.SetFromRoster(last_name  = parent[-1],
                                                         first_name = " ".join(parent[0:-1]),
-                                                        grade      = fields[2],
+                                                        grade      = grade,
                                                         teacher    = teacher,
-                                                        name_field = fields[3],
+                                                        name_field = name_field,
                                                         family_relation = "Adult"+parent_num)]
                 # prepare the parent_tag for the next parent
                 parent_count += 1
                 parent_num = str(parent_count)
 
+    def AddAdultFromDirectory(self, fields):
+        new_adult = person.Person()
+        self.adults += [new_adult.SetFromDirectory(fields)]
+    
      def AddChildFromDirectory(self, fields):
         new_child = person.Person()
         self.children += [new_child.SetFromDirectory(fields)]
     
+    def CreateFromRoster(self, fields):
+        # for elementary school (< 6th grade) teacher name is retained
+        # for middle school, teacher name is replaced with grade level
+        if int(fields[2]) < 6:
+            teacher = fields[4]
+        else:
+            teacher = fields[2]
+
+        self.AddAdultsFromCombinedField(teacher    = teacher,
+                                        name_field = fields[3],
+                                        grade      = fields[2])
+                                        
+        self.children = [new_adult.SetFromRoster(last_name  = fields[0],
+                                                 first_name = fields[1],
+                                                 grade      = fields[2],
+                                                 teacher    = teacher,
+                                                 name_field = name_field,
+                                                 family_relation = "Child1")]
+
+
     def IsSameFamily(self, other):
         if not len(self.adults) == len(other.adults):
             return False
