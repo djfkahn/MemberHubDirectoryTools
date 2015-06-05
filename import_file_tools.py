4338ca7849ca301395ed2f4b16f338fc8b753284
djfkahn <daniel.jf.kahn@gmail.com>
2015-06-05 10:53:31 -0700

    convert to using classes for new members file

--- import_file_tools.py
+++ import_file_tools.py
@@ -3,17 +3,35 @@
 imported into MemberHub.
 """
 
+import person
+import family
+
 from time import localtime, strftime
 
 def FormTimeTag():
     tag = strftime("%Y-%m-%d-%H-%M-%S", localtime())
     return tag
 
-def CreateNewMemberImport(update_d):
+def WriteNewMemberLine(open_file, family_relation, first_name, last_name, hubs, person_id):
+    line = "%s,%s,%s,%s,%s\n" % (family_relation, first_name, last_name, hubs, person_id)
+    open_file.write(line)
+
+def WriteNewMemberPerson(open_file, new_person):
+    family_relation = new_person.family_relation
+    first_name = new_person.first_name
+    last_name  = new_person.last_name
+    hubs = ""
+    for hub_id in new_person.hubs:
+        hubs = str(hub_id) + ";"
+    hubs       = hubs[:-1]  # strip off the trailing ';'
+    person_id  = ''
+    WriteNewMemberLine(open_file, family_relation, first_name, last_name, hubs, person_id)
+    
+def CreateNewMemberImport(entriless):
     """CreateHubUpdateCSV
-Inputs : update_d - a dictionary containing the first/last name, hub memberships
-                    and person ID to be updated
-Outputs: None
+Inputs : entriless - list of families that need to be added to the directory
+Outputs: Creates a file called 'new_member_import_<date tag>.csv' in the
+         run directory
 Summary: 1. asks user to name file to be written to.  this file will be
             over-written!
          2. opens file for writing, and writes the column titles
@@ -25,16 +43,13 @@ Summary: 1. asks user to name file to be written to.  this file will be
     print "Writing to import file called %s." % file_name
     try:
         open_file = open(file_name,"w")
-        open_file.write("family_relation,first_name,last_name,hubs,person_id\n")
+        WriteNewMemberLine(open_file,'family_relation', 'first_name', 'last_name', 'hubs','person_id')
 
-        for entry in update_d.keys():
-            line = "%s,%s,%s,%s,%s\n" % \
-                       (update_d[entry]["family_relation"],
-                        update_d[entry]["first_name"],
-                        update_d[entry]["last_name"],
-                        update_d[entry]["hubs"],
-                        update_d[entry]["person_id"])
-            open_file.write(line)
+        for new_family in entriless:
+            for new_adult in new_family.GetAdults():
+                WriteNewMemberPerson(open_file, new_adult)
+            for new_child in new_family.GetChildren():
+                WriteNewMemberPerson(open_file, new_child)
 
     finally:
         open_file.close()
