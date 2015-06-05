5d42dea5377fb67996cd1b2f2f213730bc955347
djfkahn <daniel.jf.kahn@gmail.com>
2015-06-05 12:24:09 -0700

    use classes to form hubless import file

--- import_file_tools.py
+++ import_file_tools.py
@@ -12,6 +12,12 @@ def FormTimeTag():
     tag = strftime("%Y-%m-%d-%H-%M-%S", localtime())
     return tag
 
+def ConvertHubListToImportString(hub_list):
+    for hub in hub_list:
+        hub_str = str(hub) + ";"
+        
+    return hub_str[:-1]  # strip off the trailing ';'
+    
 def WriteNewMemberLine(open_file, family_relation, first_name, last_name, hubs, person_id):
     line = "%s,%s,%s,%s,%s\n" % (family_relation, first_name, last_name, hubs, person_id)
     open_file.write(line)
@@ -20,10 +26,7 @@ def WriteNewMemberPerson(open_file, new_person):
     family_relation = new_person.family_relation
     first_name = new_person.first_name
     last_name  = new_person.last_name
-    hubs = ""
-    for hub_id in new_person.hubs:
-        hubs = str(hub_id) + ";"
-    hubs       = hubs[:-1]  # strip off the trailing ';'
+    hubs = ConvertHubListToImportString(new_person.hubs)
     person_id  = ''
     WriteNewMemberLine(open_file, family_relation, first_name, last_name, hubs, person_id)
     
@@ -55,31 +58,35 @@ Summary: 1. asks user to name file to be written to.  this file will be
         open_file.close()
 
 
+def WriteHublessLine(open_file, first_name, last_name, hubs, person_id):
+    line = "%s,%s,%s,%s\n" % (first_name, last_name, hubs, person_id)
+    open_file.write(line)
+
+def WriteHublessPerson(open_file, new_person):
+    first_name = new_person.first_name
+    last_name  = new_person.last_name
+    hubs = ConvertHubListToImportString(new_person.hubs)
+    person_id  = new_person.person_id
+    WriteHublessLine(open_file, first_name, last_name, hubs, person_id)
+    
 def CreateHublessImportFile(update_d):
     """CreateHublessImportFile
-Inputs : update_d - a dictionary containing the first/last name, hub memberships
-                    and person ID to be updated
-Outputs: None
-Summary: 1. asks user to name file to be written to.  this file will be
-            over-written!
-         2. opens file for writing, and writes the column titles
-         3. iterates over the inputs to write the data to the file
-         4. closes the file
+Inputs : hubless - list of persons that need their hubs updated
+Outputs: Creates a file called 'hubless_import_<date tag>.csv' in the
+         run directory
+Summary: 1. opens file for writing, and writes the column titles
+         2. iterates over the inputs to write the data to the file
+         3. closes the file
 """
  
     file_name = "hubless_import_" + FormTimeTag() + ".csv"
     print "Writing to import file called %s." % file_name
     try:
         open_file = open(file_name,"w")
-        open_file.write("first_name,last_name,hubs,person_id\n")
+        WriteHublessLine (open_file, "first_name", "last_name", "hubs", "person_id")
 
-        for entry in update_d.keys():
-            line = "%s,%s,%s,%s\n" % \
-                       (update_d[entry]["first_name"],
-                        update_d[entry]["last_name"],
-                        update_d[entry]["hubs"],
-                        update_d[entry]["person_id"])
-            open_file.write(line)
+        for update_person in hubless:
+            WriteHublessPerson(open_file, update_person)
 
     finally:
         open_file.close()
