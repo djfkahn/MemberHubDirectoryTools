5fb0985c75e3f0fd341a1d34c2ee47d1f3f1acb5
djfkahn <daniel.jf.kahn@gmail.com>
2020-09-23 20:21:09 -0700

    add script to create an iconed shortcut to the GUI

---
+++ shortcut_maker.py
@@ -0,0 +1,7 @@
+from pyshortcuts import make_shortcut
+
+make_shortcut(script   = '/Users/djfkahn/Desktop/DEV/MemberHubDirectoryTools/gui_executive.py',
+              name     = 'Husky MH Tools',
+              icon     = '/Users/djfkahn/Desktop/MH_Tools.icns',
+              desktop  = True,
+              terminal = False)
